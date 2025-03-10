3b856c45b1a7c1ba8b4ca39adaf455d73562bbe9
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2025-03-11 00:47:41 +0300

    feats : - login sayfası tamamlandı. -app.routes.ts tamamlandı. -login-moneytolia.svg eklendi.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './shared/guards/auth.guard';
+import { LoginComponent } from './pages/login/login.component';
+import { CampaignListComponent } from './pages/campaign-list/campaign-list.component';
+import { CampaignCreateComponent } from './pages/campaign-create/campaign-create.component';
+import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+
+  {
+    path: '',
+    component: MainLayoutComponent,
+    canActivate: [authGuard],
+    children: [
+      { path: '', redirectTo: 'campaign-list', pathMatch: 'full' },
+      { path: 'campaign-list', component: CampaignListComponent },
+      { path: 'create', component: CampaignCreateComponent },
+    ],
+  },
+
+  { path: '**', redirectTo: '' },
+];
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { LocalStorageService } from '../../shared/services/local-storage.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [MatCardModule, CommonModule, MatInputModule, MatButtonModule, MatFormFieldModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  fb = inject(FormBuilder);
+  router = inject(Router);
+  storage = inject(LocalStorageService);
+  mockUser = {
+    username: 'testuser',
+    password: '123456'
+  };
 
+  constructor() {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(3)]]
+    });
+  }
+
+  ngOnInit() {
+    if (this.storage.isLoggedIn()) {
+      this.router.navigate(['/campaign-list']);
+    }
+  }
+
+  get username() {
+    return this.loginForm.get('username')!;
+  }
+
+  get password() {
+    return this.loginForm.get('password')!;
+  }
+
+  login() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const { username, password } = this.loginForm.value;
+
+    if (username === this.mockUser.username && password === this.mockUser.password) {
+      this.storage.setLoggedIn(true);
+      this.router.navigate(['/campaign-list']);
+    } else {
+      alert('Kullanıcı adı veya şifre yanlış!');
+    }
+  }
 }
